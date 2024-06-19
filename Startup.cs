bce616ee5b7d3c02f7d90cef71295c3f52d6a25c
Kolovratik <Kolovratik@HOME-PC>
2024-06-19 21:26:54 +0300

    Настройка приложения документации, добавление версии документации.

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,12 @@ namespace PR45
                     Title = "Руководство для использования запросов",
                     Description = "Полное руководство для использования запросов находящихся в проекте"
                 });
+                c.SwaggerDoc("v4", new OpenApiInfo
+                {
+                    Version = "v4",
+                    Title = "Руководство для использования запросов",
+                    Description = "Полное руководство для использования запросов находящихся в проекте"
+                });
                 var filePath = Path.Combine(AppContext.BaseDirectory, "ASP_GET.xml");
                 c.IncludeXmlComments(filePath);
             });
@@ -51,6 +57,7 @@ namespace PR45
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Запросы GET");
                 c.SwaggerEndpoint("/swagger/v2/swagger.json", "Запросы POST");
                 c.SwaggerEndpoint("/swagger/v3/swagger.json", "Запросы PUT");
+                c.SwaggerEndpoint("/swagger/v4/swagger.json", "Запросы DELETE");
             });
         }
     }
